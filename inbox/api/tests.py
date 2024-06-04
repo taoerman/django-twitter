3c7c09f52b76dac99c3bebc38cdfe9b2865b2066
taoerman <qiliu0523@gmail.com>
2024-06-04 14:26:37 -0700

    notification api part2

--- inbox/api/tests.py
+++ inbox/api/tests.py
@@ -109,3 +109,46 @@ class NotificationApiTests(TestCase):
         self.assertEqual(response.data['count'], 1)
         response = self.linghu_client.get(NOTIFICATION_URL, {'unread': False})
         self.assertEqual(response.data['count'], 1)
+
+    def test_update(self):
+        self.dongxie_client.post(LIKE_URL, {
+            'content_type' : 'tweet',
+            'object_id' : self.linghu_tweet.id,
+        })
+        comment = self.create_comment(self.linghu, self.linghu_tweet)
+        self.dongxie_client.post(LIKE_URL, {
+            'content_type' : 'comment',
+            'object_id' : comment.id,
+        })
+        notification = self.linghu.notifications.first()
+
+        url = '/api/notifications/{}/'.format(notification.id)
+        # post is not allowed
+        response = self.dongxie_client.post(url, {
+            'unread' : False,
+        })
+        self.assertEqual(response.status_code, 405)
+        # others can not change the status of notifications
+        response = self.anonymous_client.put(url, {'unread' : False})
+        self.assertEqual(response.status_code, 403)
+
+        response = self.dongxie_client.put(url, {'unread' : False})
+        self.assertEqual(response.status_code, 404)
+
+        response = self.linghu_client.put(url, {'unread' : False})
+        self.assertEqual(response.status_code, 200)
+        unread_url = '/api/notifications/unread-count/'
+        response = self.linghu_client.get(unread_url)
+        self.assertEqual(response.data['unread_count'], 1)
+
+
+        response = self.linghu_client.put(url, {'unread' : True})
+        response = self.linghu_client.get(unread_url)
+        self.assertEqual(response.data['unread_count'], 2)
+
+        response = self.linghu_client.put(url, {'verb' : 'newberb'})
+        self.assertEqual(response.status_code, 400)
+
+        response = self.linghu_client.put(url, {'verb' : 'newverb', 'unread' : False})
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.data['verb'], 'newverb')
