77e6a2b6efb155528ae44a29e58925158e480e96
taoerman <qiliu0523@gmail.com>
2024-06-10 22:10:25 -0700

    tweet cache

--- newsfeeds/models.py
+++ newsfeeds/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tweets.models import Tweet
+from utils.memcached_helper import MemcachedHelper
 
 class NewsFeed(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
@@ -14,3 +15,8 @@ class NewsFeed(models.Model):
 
     def __str__(self):
         return f'{self.created_at} inbox of {self.user} : {self.tweet}'
+
+    @property
+    def cached_tweet(self):
+        return MemcachedHelper.get_object_through_cache(Tweet, self.tweet_id)
+
--- tweets/models.py
+++ tweets/models.py
@@ -4,7 +4,9 @@ from utils.time_helpers import utc_now
 from likes.models import Like
 from django.contrib.contenttypes.models import ContentType
 from tweets.constants import TweetPhotoStatus, TWEET_PHOTO_STATUS_CHOICES
-from accounts.services import UserService
+from utils.memcached_helper import MemcachedHelper
+from django.db.models.signals import post_save, pre_delete
+from utils.listeners import invalidate_object_cache
 
 
 class Tweet(models.Model):
@@ -42,7 +44,7 @@ class Tweet(models.Model):
 
     @property
     def cached_user(self):
-        return UserService.get_user_through_cache(self.user_id)
+        return MemcachedHelper.get_object_through_cache(User, self.user_id)
 
 
 class TweetPhoto(models.Model):
@@ -83,3 +85,6 @@ class TweetPhoto(models.Model):
 
     def __str__(self):
         return f'{self.tweet.id} : {self.file}'
+
+pre_delete.connect(invalidate_object_cache, sender=Tweet)
+post_save.connect(invalidate_object_cache, sender=Tweet)
