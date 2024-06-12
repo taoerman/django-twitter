36c1aa0781b81441f56aa5ca8289f7138385fa4f
taoerman <qiliu0523@gmail.com>
2024-06-12 14:57:45 -0700

    newsfeeds cache

--- newsfeeds/services.py
+++ newsfeeds/services.py
@@ -1,9 +1,11 @@
 from friendships.services import FriendshipService
 from newsfeeds.models import NewsFeed
+from twitter.cache import USER_NEWSFEEDS_PATTERN
+from utils.redis_helper import RedisHelper
 class NewsFeedService(object):
 
     @classmethod
-    def fanout_to_follower(self, tweet):
+    def fanout_to_follower(cls, tweet):
         # followers = FriendshipService.get_followers(tweet.user)
         # we can not use forloop + query
         # for follower in followers:
@@ -18,3 +20,18 @@ class NewsFeedService(object):
         ]
         newsfeeds.append(NewsFeed(user=tweet.user, tweet=tweet))
         NewsFeed.objects.bulk_create(newsfeeds)
+
+        for newsfeed in newsfeeds:
+            cls.push_newsfeeds_to_cache(newsfeed)
+
+    @classmethod
+    def get_cached_newsfeeds(cls, user_id):
+        queryset = NewsFeed.objects.filter(user_id=user_id).order_by('-created_at')
+        key = USER_NEWSFEEDS_PATTERN.format(user_id=user_id)
+        return RedisHelper.load_objects(key, queryset)
+
+    @classmethod
+    def push_newsfeeds_to_cache(cls, newsfeed):
+        queryset = NewsFeed.objects.filter(user_id=newsfeed.user_id).order_by('-created_at')
+        key = USER_NEWSFEEDS_PATTERN.format(user_id=newsfeed.user_id)
+        RedisHelper.push_object(key, newsfeed, queryset)
